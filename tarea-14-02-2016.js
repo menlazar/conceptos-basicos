8bb3a9a3f6226ac1a7eebe2688b3f68d786a8f6f
Fernando Barrios <fer.b.1990@gmail.com>
2016-02-14 17:25:35 -0600

    tarea dejada el 14 de febrero del 2016

---
+++ tarea-14-02-2016.js
@@ -0,0 +1,24 @@
+// practicando con tipos de variables
+
+// EJERCICIO 1:
+/**
+ * Crear un array que contenga los números del 1 al 10.
+ * Después imprimir la posición que contenga el número 1, 4, 6, 10.
+ */
+
+// EJERCICIO 2:
+/**
+ * Crear un objeto de tipo persona que contenga todas las propiedades de una persona
+ * como nombre, apellido, edad, DPI
+ *
+ * Imprimir en la consola un saludo de la forma: Hola {NOMBRE} {APELLIDO}, te recuerdo que tu DPI es
+ * {DPI}
+ */
+
+//EJERCICIO 3:
+/**
+ * crear N cantidad de variables y luego imprimirlas de forma que en la consola
+ * de nodejs se muestre lo siguiente: A1B2C3D4E5-1234567890
+ *
+ * Nota: No es posible crear más de dos carácteres por variable
+ */
