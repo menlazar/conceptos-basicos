eb66e56db3c0deb5a947b109765e5bc32a116508
Iván Méndez <ivan@menlazar.de>
2016-02-13 19:28:32 -0600

    Linea 25: mostrar que tipo de variable estamos utilizando (number, string, object) Linea 28: ejemplo donde la funcion "suma" se refiere a "concatenar", no exactamente es una suma, si no que hace una union de los objetos que llamamos. Linea 29: se demuestra donde "concatenar" hace exactamente lo mismo que "sumar" Linea 31: Demostracion de como la funcion "contact" utiliza los datos de "var sNumero ='12' " junto con los datos numericos ( ) Linea 32: impresion de la suma (que al mismo tiempo es concatenar) de la variable concatenarV2. Linea 34: impresion donde la variable "texto" se le define que caracter mostrar (siempre tomando en cuenta la clave de cada caracter; empezando en 0).
    Recordar que todo en JS son objetos, string son arrays y objectos.

--- aprender-variables.js
+++ aprender-variables.js
@@ -1,6 +1,7 @@
 // Aprender sobre variables y objetos
 
 var numero = 12;
+var sNumero = '12';
 var texto = 'texto';
 
 var arr = [12,30,50,62,39, 'hola'];
@@ -19,3 +20,15 @@ var persona = {
 
 // console.log('nombre: ' + persona.nombre);
 // console.log('Edad: ' + persona.edad);
+
+//muestra el tipo de variable que es
+// console.log(typeof persona);
+
+
+// var suma = numero + 2;
+// var concatenar = sNumero + 2;
+
+// var concatenarV2 = sNumero.concat(2, 3, 4, 5);
+// console.log('suma: ', concatenarV2);
+
+console.log(texto[0]);
