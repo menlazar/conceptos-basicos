4aa9a40de676bc926a08211dc833e61ea0a2d2e9
Iván Méndez <ivan@menlazar.de>
2016-02-13 19:05:34 -0600

    Linea 1: Se insertan // para realizar un comentario de texto Linea 3: Se crea una variable numerica Linea 4: Variable de texto, siempre se debe utilizar comillas simples ( ' ' )  para definir texto Linea 6: Se hace un Array para asignar valores a varias claves (siempre empezando en clave 0) Linea 9: array con datos como nombre, apellido y edad con su respectiva todos son valores y clave empieza en 0 Linea 11: Objecto de Persona asignando como clave nombre, apellido y edad con su respectivo valor.
    Lineas 17 a 21:Console.log() para mostrar en consola para mostrar como funciona cada Variable.

---
+++ aprender-variables.js
@@ -0,0 +1,21 @@
+// Aprender sobre variables y objetos
+
+var numero = 12;
+var texto = 'texto';
+
+var arr = [12,30,50,62,39, 'hola'];
+// clanes   0  1  2  3  4     5
+
+var datosIvan = ['ivan', 'mendez', 21];
+
+var persona = {
+	nombre: 'Ivan',
+	apellido: 'Mendez',
+	edad: 21
+};
+
+// console.log('nombre: ' + datosIvan[0]);
+// console.log('Apellido: ' + datosIvan[1]);
+
+// console.log('nombre: ' + persona.nombre);
+// console.log('Edad: ' + persona.edad);
