94a526835bba4b3f054d84de712539b0bbff8486
fbarrios <fbarrios@stsa.info>
2016-02-19 07:43:22 -0600

    tarea del 18 de febrero 2016

---
+++ tarea-18-02-2016.js
@@ -0,0 +1,15 @@
+// EJERCICIO 1:
+
+/**
+ * Leer sobre los ciclos: for, forEach, do while y while
+ * y Hacer un ejemplo de cada uno explicando que hace cada linea de codigo
+ */
+
+
+// EJERCICIO 2:
+
+/**
+ * Crear N cantidad de arrays donde N debe ser el menor numero posible, luego
+ * utilizar el ciclo for que recorra los arrays para poder 
+ * imprimir: A1B2C3D4E5-1234567890
+ */
