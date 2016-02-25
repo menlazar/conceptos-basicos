23ec27d5d0c0b665056f7df0d026b5c2c3dbbe46
Fernando Barrios <fer.b.1990@gmail.com>
2016-02-24 22:07:17 -0600

    tarea 24 febrero 2016

---
+++ tarea-24-02-2016.js
@@ -0,0 +1,29 @@
+// EJERCICIO 1.
+
+/**
+ * Utilizar los ciclos forEach con N cantidad de arrays donde N sea
+ * el menor numero posible para imprimir 
+ * A9B8C7D6E5+123654789
+ */
+
+// EJERCICIO 2.
+/**
+ * Leer sobre el ciclo for in para objetos de la forma:
+ * var obj = {
+ *   nombre: "Fernando",
+ *   apellido: "Barrios",
+ *   edad: 25,
+ *   estado: "soltero"
+ * };
+ * for (var i in obj )
+ */
+
+// EJERCICIO 3
+/**
+ * Crear una función que reciba dos números y que devuelva la suma de ambos
+ */
+
+// EJERCICIO 4
+/**
+ * Crear una función que reciba un array y que devuela la suma de sus elementos
+ */
