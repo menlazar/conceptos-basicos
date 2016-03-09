91677fbfcdefa7a0b6bc3f0386d2d0cfceaa7ce3
Fernando Barrios <fer.b.1990@gmail.com>
2016-03-09 09:04:51 -0600

    tarea 09-03-2016

---
+++ tarea-09-03-2016.js
@@ -0,0 +1,23 @@
+//Ejercicio 1
+/**
+ * Realizar una pagina web donde se pueda mostrar los números primos de 1 a 200
+ */
+
+
+// Ejercicio 2
+/**
+ * Crear conversor de monedas usando un objeto que contenga un listado de monedas
+ * y su taza de cambio contra el quetzal...
+ * Necesito que sea una funcion que yo le pueda pasar los parametros:
+ * moneda y monto (en quetzales) y me devuelva el monto (en la moneda que he pasado de parametro)
+ */
+
+// Ejercicio 3
+/**
+ * Calculadora
+ * Realizar una calculadora que reciba como parametros dos numeros y una operación
+ * las operaciones posibles son todas las que tiene disponible Javascript en su objeto Math
+ * Primer resultado de google: http://artistasdigitales.com/javascript-operaciones-y-funciones-matematicas-basicas/
+ * mejor sitio según mi criterio: http://devdocs.io/javascript/global_objects/math
+ * Segundo mejor sitio: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Math
+ */
