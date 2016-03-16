7b9dd39cf6164a90dc174edba2d93efe8efdc437
Iván Méndez <ivan@menlazar.de>
2016-03-15 22:45:55 -0600

    Tarea del 15-03-2016

---
+++ tarea-15-03-2016.js
@@ -0,0 +1,33 @@
+/* Ejercicio 1:
+ * mostrar el factorial de cualquier numero que yo ingrese*/
+
+/* function factorial (x) {
+  var total = 1;
+    for(var i=1; i<=x; i++) {
+      total *=i;
+    }
+    console.log(total);
+ }
+ factorial(4);*/
+
+// Ejercicio 2:
+ /** Mostrar todos los números primos que su último dígito sea el 3
+ * por ejemplo
+ * 3, 13, 23*/
+ 
+/*var numInicial = 2;
+do {
+      var mods = 0;
+      for (var i=1; i <= numInicial; i++) {
+        if (numInicial%i === 0) {
+          mods++;
+        }
+      }
+      if (mods === 2) {
+        numString = numInicial + '';
+        if (numString.charAt(numString.length-1) === '3') {
+          console.log(numInicial);
+        }  
+      }
+      numInicial++;
+    } while (numInicial < 50);*/
